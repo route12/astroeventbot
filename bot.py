d789f1fb184eabc33bffccf944e8ea7a8b2351d0
t_issy <37074098+route12@users.noreply.github.com>
2025-09-13 23:57:23 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,24 @@ from requests_oauthlib import OAuth1
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
+w = ["月", "火", "水", "木", "金", "土", "日"]
 japan_tz = ZoneInfo("Asia/Tokyo")
 today = datetime.now(japan_tz)
-next_year = today.year + 1
-new_years_day = datetime(next_year, 1, 1, tzinfo=japan_tz)
-days_remaining = (new_years_day - today).days
-message = "本日は" + today.strftime("%Y年%m月%d日") + "です。\n"
-message += "来年まで残り" + str(days_remaining) + "日です。"
-
+message = "本日は" + today.strftime("%Y/%m/%d") + "(" + w[today.weekday()] + ")です。\n"
+url = "https://star.static.jp/data/calendar.csv"
+rows = requests.get(url).content.decode("utf-8").splitlines()
+for row in rows:
+    v = row.split(",")
+    y = int(v[0])
+    m = int(v[1])
+    d = int(v[2])
+    dt = datetime(y, m, d, tzinfo=japan_tz)
+    name = v[3] + "の" + dt.strftime("%Y/%m/%d") + "(" + w[dt.weekday()] + ")"
+    dd = (dt - today).days
+    if dd > 0:
+        message += name + "まで残り" + str(dd + 1) + "日です。\n"
+    if dd > 29:
+        break
 api_key = os.getenv("X_API_KEY")
 api_secret = os.getenv("X_API_SECRET")
 access_token = os.getenv("X_ACCESS_TOKEN")
