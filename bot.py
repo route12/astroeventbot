4b38ae4df8094470e45c19dfa8011edc291a7e81
t_issy <37074098+route12@users.noreply.github.com>
2025-09-13 20:52:16 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,16 @@
-# bot.py
-import requests
 import os
+import requests
+from requests_oauthlib import OAuth1
 
-# 環境変数からトークン取得
-BEARER_TOKEN = os.getenv("X_BEARER_TOKEN")
+api_key = os.getenv("X_API_KEY")
+api_secret = os.getenv("X_API_SECRET")
+access_token = os.getenv("X_ACCESS_TOKEN")
+access_token_secret = os.getenv("X_ACCESS_TOKEN_SECRET")
 
 url = "https://api.x.com/2/tweets"
-payload = {"text": "こんにちは！GitHub Actionsからの定期投稿です。"}
-headers = {
-    "Authorization": f"Bearer {BEARER_TOKEN}",
-    "Content-Type": "application/json"
-}
+payload = {"text": "GitHub Actionsから投稿テストです！"}
 
-response = requests.post(url, json=payload, headers=headers)
-print(response.status_code, response.text)
+auth = OAuth1(api_key, api_secret, access_token, access_token_secret)
+
+res = requests.post(url, json=payload, auth=auth)
+print(res.status_code, res.text)
