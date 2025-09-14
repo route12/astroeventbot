d0fe2be354e038f4608756420c47bc934196cbbb
t_issy <37074098+route12@users.noreply.github.com>
2025-09-14 14:36:59 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,16 @@
 import os
 import requests
 from requests_oauthlib import OAuth1
-from datetime import datetime
-from zoneinfo import ZoneInfo
+from playwright.sync_api import sync_playwright
 
-w = ["月", "火", "水", "木", "金", "土", "日"]
-japan_tz = ZoneInfo("Asia/Tokyo")
-today = datetime.now(japan_tz)
-message = "本日は" + today.strftime("%Y/%m/%d") + "(" + w[today.weekday()] + ")です。\n"
-url = "https://star.static.jp/data/calendar.csv"
-rows = requests.get(url).content.decode("utf-8").splitlines()
-for row in rows:
-    v = row.split(",")
-    y = int(v[0])
-    m = int(v[1])
-    d = int(v[2])
-    dt = datetime(y, m, d, tzinfo=japan_tz)
-    name = v[3] + "の" + dt.strftime("%Y/%m/%d") + "(" + w[dt.weekday()] + ")"
-    dd = (dt - today).days
-    if dd > 0:
-        message += name + "まで残り" + str(dd + 1) + "日です。\n"
-    if dd > 30:
-        break
+message = ""
+url = "https://star.static.jp/data/event.html"
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+    page.goto(url, timeout=60000)
+    message += page.inner_text("body")
+    browser.close()
 api_key = os.getenv("X_API_KEY")
 api_secret = os.getenv("X_API_SECRET")
 access_token = os.getenv("X_ACCESS_TOKEN")
